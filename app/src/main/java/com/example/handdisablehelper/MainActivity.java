6f35b9f1b6218314f57797cdf316b4b032c7d320
quangchinh186 <quangchinh1122@gmail.com>
2024-05-06 15:28:34 +0700

    added opencv module and open camera

--- app/src/main/java/com/example/handdisablehelper/MainActivity.java
+++ app/src/main/java/com/example/handdisablehelper/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.handdisablehelper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends CameraActivity {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getCameraPermission();
+
+        cameraBridgeViewBase = findViewById(R.id.openCVCamera);
+
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+
+            }
+
+            @Override
+            public void onCameraViewStopped() {
+
+            }
+
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+                return inputFrame.rgba();
+            }
+        });
+
+        if(OpenCVLoader.initDebug()) {
+            Log.v("OpenCV", "Success");
+            cameraBridgeViewBase.enableView();
+        }
+        else Log.v("OpenCV", "err");
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(cameraBridgeViewBase);
+    }
+
+    void getCameraPermission(){
+        if(checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            getCameraPermission();
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "HandDisableHelper"
 include(":app")
- 
+include(":openCV")
